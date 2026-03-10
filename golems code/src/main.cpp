fb48efe5fb2a6443c257dfa0a443145861dc710a
joelmathewsssss <joelalexmat@gmail.com>
2026-03-10 13:19:19 -0400

    init

--- golems code/src/main.cpp
+++ golems code/src/main.cpp
@@ -6,16 +6,22 @@ int main()
 {
     const Color darkGreen = {20, 160, 133, 255};
     
-    constexpr int screenWidth = 800;
-    constexpr int screenHeight = 600;
+    constexpr int screenWidth = 1000;
+    constexpr int screenHeight = 800;
     
     InitWindow(screenWidth, screenHeight, "Golems");
     SetTargetFPS(8);
+    
+
+    Image image = LoadImage("assets/golems game art.jpg"); // Load the image
+    Texture2D texture = LoadTextureFromImage(image);   // Convert image to texture
+    UnloadImage(image); 
     
     while (!WindowShouldClose())
     {
         
         BeginDrawing();
+            DrawTexture(texture, 100, 100, WHITE); // Draw at (100, 100)
             ClearBackground(darkGreen);
         EndDrawing();
     }
