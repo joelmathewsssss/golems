81a5ed3da8c6b33bddd3322a67f4b5e92e602978
joelmathewsssss <joelalexmat@gmail.com>
2026-03-10 21:54:04 -0400

    set

--- golems code/src/main.cpp
+++ golems code/src/main.cpp
@@ -1,10 +1,15 @@
 #include <raylib.h>
 #include <iostream>
+
+
 using namespace std;
 
 int main() 
-{
-    const Color darkGreen = {20, 160, 133, 255};
+{   
+    const Color darkGreen = {20, 160, 133, 255}; // Custom dark green color
+    
+    constexpr int artWidth = 800;
+    constexpr int artHeight = 400;
     
     constexpr int screenWidth = 1000;
     constexpr int screenHeight = 800;
@@ -14,6 +19,7 @@ int main()
     
 
     Image image = LoadImage("assets/golems game art.jpg"); // Load the image
+        ImageResize(&image, artWidth, artHeight);
     Texture2D texture = LoadTextureFromImage(image);   // Convert image to texture
     UnloadImage(image); 
     
@@ -21,11 +27,15 @@ int main()
     {
         
         BeginDrawing();
-            DrawTexture(texture, 100, 100, WHITE); // Draw at (100, 100)
+        
             ClearBackground(darkGreen);
-        EndDrawing();
+            int artX = (screenWidth - artWidth) / 2;
+            int artY = (screenHeight - artHeight) / 2;
+            DrawTexture(texture, artX, artY, WHITE);
+            EndDrawing();
     }
     
+        UnloadTexture(texture);
     CloseWindow();
 
     return 0;
